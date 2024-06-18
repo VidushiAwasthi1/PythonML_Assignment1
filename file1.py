734346b25139388638fccda35bf4ba693c5af766
Vidushi Awasthi <144840188+VidushiAwasthi1@users.noreply.github.com>
2024-06-18 23:45:04 +0530

    Update file1.py

--- file1.py
+++ file1.py
@@ -1,3 +1,4 @@
-d = {'a': 1, 'b': 2, 'c': 3}
-d.update({'b': 5})
-print(d)
+#Write a program that takes two numbers as input and prints their sum. 
+num1= int(input("Enter a number: "))
+num2= int(input("Enter a number: "))
+print(num1+num2)

8db8e855275c0b9a10c214936e0c54b46e6f8c38
Vidushi Awasthi <144840188+VidushiAwasthi1@users.noreply.github.com>
2024-06-19 00:06:33 +0530

    Update f9.py

---
+++ f9.py
@@ -0,0 +1,7 @@
+#Write a python program that checks if a substring is present in a given string.
+string=input("Enter a string: ")
+substring=input("Enter a substring: ")
+if(substring in string):
+    print("Subtring is present in given string")
+else:
+    print("Subtring is not present in given string")

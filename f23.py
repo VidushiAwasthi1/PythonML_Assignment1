be10eef86f951ebfa504e4ee96dd09ef32e72042
Vidushi Awasthi <144840188+VidushiAwasthi1@users.noreply.github.com>
2024-06-20 05:35:25 +0530

    Update f23.py

--- f23.py
+++ f23.py
@@ -1,1 +1,12 @@
+#Write a program that converts temperature from Celsius to Fahrenheit and vice versa based on user input. 
+temp=input("Enter the temperature: ")
+if temp[-1]=='C':
+    res=int(temp[:-2])*9/5 +32
+    var='F'
+elif temp[-1]=='F':
+    res=(int(temp[:-2])-32)*5/9
+    var='C'
+else:
+    print("invalid input")
+print("Resulting temp is",res,var)
 
